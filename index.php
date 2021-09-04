7c6769e12db45b52611a740b8ff4f86b98963d16
Sokng <sok_ng@yahoo.com>
2021-09-04 18:23:31 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,23 +4,7 @@
 </head>
 <body>
 <?php
-require_once "Classes/PHPExcel.php";
-
-		$tmpfname = "test.xlsx";
-		$excelReader = PHPExcel_IOFactory::createReaderForFile($tmpfname);
-		$excelObj = $excelReader->load($tmpfname);
-		$worksheet = $excelObj->getSheet(0);
-		$lastRow = $worksheet->getHighestRow();
-		
-		echo "<table>";
-		for ($row = 1; $row <= $lastRow; $row++) {
-			 echo "<tr><td>";
-			 echo $worksheet->getCell('A'.$row)->getValue();
-			 echo "</td><td>";
-			 echo $worksheet->getCell('B'.$row)->getValue();
-			 echo "</td><tr>";
-		}
-		echo "</table>";	
+echo("Hello World");
 ?>
 </body>
 </html>
